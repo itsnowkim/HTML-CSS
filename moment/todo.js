08290fae78f7ad1d5edafc9cbc1f8885e9f32a3d
Hyunjae Kim <peterhyunjae@naver.com>
2020-03-10 20:51:08 +0900

    not finished

--- moment/todo.js
+++ moment/todo.js
@@ -2,18 +2,35 @@ const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
-const TODO_LS = "toDo";
-const toDo = new Array();
+const TODO_LS = "toDo";//key value
+let toDo = [];//storage array that save the work to do
+
+function deleteToDo(event){
+    //remove from html
+    const li = event.target.parentNode;
+    toDoList.removeChild(li);
+
+    //remove from database
+    //Returns the elements of an array that meet the condition specified in a callback function.
+
+    const cleanToDo = toDo.filter(function(toDo){
+        return toDo.id != parseInt(li.id);
+    });
+
+    toDo = cleanToDo;
+    saveToDo(toDo);
+}
 
 function saveToDo(toDo){
-    console.log(toDo);
-    localStorage.setItem(TODO_LS,toDo.text);
+    //console.log(toDo);
+    localStorage.setItem(TODO_LS,JSON.stringify(toDo));
 }
 
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerHTML = "❌";
+    delBtn.addEventListener("click",deleteToDo);
     const span = document.createElement("span");
     span.innerText = text;
     const newId = toDo.length + 1;
@@ -43,12 +60,19 @@ function loadToDo(){
 
     if(loadedToDo != null){
         const parsedToDo = JSON.parse(loadedToDo);
-        console.log(loadedToDo);
+        parsedToDo.forEach(function(a){
+            //console.log(a.text);
+            paintToDo(a.text);
+        });
+        // parsedToDo.forEach(function(toDo){
+        //     paintToDo(toDo.text);
+        // });
+
     }
 }
 
 function init(){
-    //loadToDo();
+    loadToDo();
     toDoForm.addEventListener("submit",handleSubmit);
 }
 
